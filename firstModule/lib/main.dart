95749ce39a8de7e00d6a69929cd6c25e533fdc86
DefinitelynotZombie <davidmuoegbunam@gmail.com>
2024-09-14 23:22:06 +0100

    same with the second_module just for debugging lessons

--- firstModule/lib/main.dart
+++ firstModule/lib/main.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:basics_first_app/gradient_container.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       home: Scaffold(
-        body: GradientContainer( Color.fromARGB(211, 85, 111, 198),
-             Color.fromARGB(255, 255, 82, 2)),
+        body: GradientContainer(
+            Color.fromARGB(211, 85, 111, 198), Color.fromARGB(255, 255, 82, 2)),
       ),
-    ),
-  );
+    );
+  }
 }
---
+++ second_module_debug_lessons/android/app/src/main/kotlin/com/example/second_module_debug_lessons/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.second_module_debug_lessons
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
